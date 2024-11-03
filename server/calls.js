2893570421539d36d4f1ac45362363b489e3f8fe
Ryan <ryansheikh003@gmail.com>
2024-11-03 16:39:38 -0700

    docs(api): add JSDoc comments for API call functions
    - Added detailed JSDoc comments for all API call functions including addNewBookCall, editBookCall, deleteBookCall, getFilteredBooksCall, getAuthorsCall, and getBooksGenreCall

--- server/calls.js
+++ server/calls.js
@@ -1,5 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 
+/**
+ * Sends a request to add a new book to the inventory.
+ * 
+ * @param {Object} book - The book object containing details to be added.
+ * @returns {Promise<Object>} - The response from the API call.
+ */
 export const addNewBookCall = async (book) => {
     console.log(book);
     try {
@@ -8,17 +14,29 @@ export const addNewBookCall = async (book) => {
     } catch (error) {
         return error;
     }
-}
+};
 
+/**
+ * Sends a request to edit an existing book's details in the inventory.
+ * 
+ * @param {Object} book - The book object containing updated details.
+ * @returns {Promise<Object>} - The response from the API call.
+ */
 export const editBookCall = async (book) => {
-    try{
+    try {
         const res = await axios.post('/api/edit-book/', book);
         return res;
     } catch (error) {
         return error;
     }
-}
+};
 
+/**
+ * Sends a request to delete a book from the inventory.
+ * 
+ * @param {number} entry_id - The ID of the book entry to be deleted.
+ * @returns {Promise<Object>} - The response from the API call.
+ */
 export const deleteBookCall = async (entry_id) => {
     try {
         const res = await axios.post('/api/delete-book/', { entry_id });
@@ -26,8 +44,14 @@ export const deleteBookCall = async (entry_id) => {
     } catch (error) {
         return error;
     }
-}
+};
 
+/**
+ * Sends a request to retrieve a list of books filtered by specified criteria.
+ * 
+ * @param {Object} search - An object containing search parameters for filtering.
+ * @returns {Promise<Object>} - The response from the API call containing filtered books.
+ */
 export const getFilteredBooksCall = async (search) => {
     try {
         console.log(search);
@@ -36,8 +60,13 @@ export const getFilteredBooksCall = async (search) => {
     } catch (error) {
         return error;
     }
-}
+};
 
+/**
+ * Sends a request to retrieve a list of unique authors from the inventory.
+ * 
+ * @returns {Promise<Object>} - The response from the API call containing authors.
+ */
 export const getAuthorsCall = async () => { 
     try {
         const res = await axios.post('/api/get-authors/');
@@ -45,8 +74,13 @@ export const getAuthorsCall = async () => {
     } catch (error) {
         return error;
     }
-}
+};
 
+/**
+ * Sends a request to retrieve a list of unique genres from the inventory.
+ * 
+ * @returns {Promise<Object>} - The response from the API call containing genres.
+ */
 export const getBooksGenreCall = async () => {
     try {
         const res = await axios.post('/api/get-books-genre/');
@@ -54,4 +88,4 @@ export const getBooksGenreCall = async () => {
     } catch (error) {
         return error;
     }
-}
+};
